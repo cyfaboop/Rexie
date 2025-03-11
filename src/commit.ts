bc26ed92ccf2f21cab4be83d1b391ca9950f6e2a
wooloo <ayan0312000@gmail.com>
2025-03-11 14:33:40 +0800

    refactor: rename commitDeletion to deleteFiber for clarity and consistency

--- src/commit.ts
+++ src/commit.ts
@@ -14,7 +14,7 @@ export function commitWork(fiber?: FiberFinish) {
         if (fiber.tag & Tag.PLACEMENT) {
             placeNode(fiber.parentNode, fiber.node, fiber.old?.node)
             if (fiber.old) {
-                commitDeletion(fiber.old)
+                deleteFiber(fiber.old)
             }
         }
         if (fiber.tag & Tag.UPDATE) {
@@ -32,14 +32,14 @@ export function commitWork(fiber?: FiberFinish) {
 }
 
 function commitDeletions(fiber: FiberFinish) {
-    fiber.deletions?.forEach(deletion => commitDeletion(deletion))
+    fiber.deletions?.forEach(deletion => deleteFiber(deletion))
     fiber.deletions = undefined
 }
 
-function commitDeletion(fiber: FiberFinish) {
+function deleteFiber(fiber: FiberFinish) {
     if (fiber.fc) {
         fiber.hooks && unmountEffects(fiber.hooks[HookType.List])
-        fiber.children?.forEach(commitDeletion)
+        fiber.children?.forEach(deleteFiber)
     } else {
         removeNode(fiber.parentNode, fiber.node)
         attachRef(fiber.ref, undefined)
@@ -47,16 +47,18 @@ function commitDeletion(fiber: FiberFinish) {
 }
 
 function attachRef(ref?: Ref, node?: RexieNode) {
-    if (!ref) return
-    isFunction(ref) ? ref(node) : (ref.current = node)
+    if (ref) {
+        isFunction(ref) ? ref(node) : (ref.current = node)
+    }
 }
 
 function commitHookEffects(fiber: Fiber) {
-    if (!fiber.hooks) return
-    updateEffects(fiber.hooks[HookType.Layout])
-    startTransition(
-        () => fiber.hooks && updateEffects(fiber.hooks[HookType.Effect]),
-    )
+    if (fiber.hooks) {
+        updateEffects(fiber.hooks[HookType.Layout])
+        startTransition(
+            () => fiber.hooks && updateEffects(fiber.hooks[HookType.Effect]),
+        )
+    }
 }
 
 function updateEffects(effects: HookState[]) {
