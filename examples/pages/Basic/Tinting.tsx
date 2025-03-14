b6b0c6b8e423b6285f877793ebb2c287d7738a2a
wooloo <ayan0312000@gmail.com>
2025-03-14 16:59:14 +0800

    docs: update examples

--- examples/pages/Basic/Tinting.tsx
+++ examples/pages/Basic/Tinting.tsx
@@ -1,55 +1,24 @@
 import * as PIXI from 'pixi.js'
-import {
-    h,
-    memo,
-    useRef,
-    useEffect,
-    useTransition,
-    useContext,
-    useCallback,
-    FC,
-} from 'rexie'
+import { h, memo, useRef, useEffect, useTransition, FC } from 'rexie'
 
-import { AppContext } from 'examples'
 import { Loading } from '../../components/Loading'
 
 export const Tinting: FC<{
     screen: { width: number; height: number }
 }> = memo(({ screen }) => {
-    const app = useContext(AppContext)
     const texture = useRef<PIXI.Texture>()
     const container = useRef<PIXI.Container>()
     const [isPending, startTransitioin] = useTransition()
-    const animate = useCallback(
-        (time: PIXI.Ticker) => {
-            // Continuously rotate the container!
-            // * use delta to create frame-independent transform *
-            if (container.current) {
-                container.current.rotation -= 0.01 * time.deltaTime
-                container.current.pivot
-            }
-        },
-        [container.current],
-    )
 
     useEffect(() => {
         startTransitioin(async () => {
             texture.current = await PIXI.Assets.load(
                 'https://pixijs.com/assets/eggHead.png',
             )
-            app?.ticker.add(animate)
         })
 
-        return () => {
-            app?.ticker.remove(animate)
-        }
+        return () => {}
     }, [])
-
-    useEffect(() => {
-        if (!container.current) return
-        container.current.pivot.x = container.current.width / 2
-        container.current.pivot.y = container.current.height / 2
-    }, [container.current])
 
     return (
         <container>
